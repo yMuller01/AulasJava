da1a6f05b22e5663e9335d8958e1a42119900d6d
yMuller01 <henriquemullercs@gmail.com>
2023-09-19 19:38:34 -0300

    Update EX15.java

--- Class/EX15.java
+++ Class/EX15.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Quinze {
+public class EX15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < 12; i++) {
